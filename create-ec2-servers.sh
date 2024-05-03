27911d295622b559734b11e5c13d0b9d8be3a307
VS Charan <138986928+VootlaSaiCharan@users.noreply.github.com>
2024-05-03 22:36:48 +0530

    Update create-ec2-servers.sh  updated zone_id and Domain name

--- create-ec2-servers.sh
+++ create-ec2-servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z103214126L48SQW30RSR"
-DOMAIN="devopsb71.online"
+ZONE_ID="Z05174041KI1KF2YCVAZF"
+DOMAIN="devopsbatch.cloud"
 SG_NAME="allow-all"
 #############################
 
